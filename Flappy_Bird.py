b84d986f5379330cc44ed5b4ef6acaed13fb6d52
Martin <wainainam008@gmail.com>
2026-02-04 17:36:53 +0300

    bird movement

--- Flappy_Bird.py
+++ Flappy_Bird.py
@@ -1,37 +1,50 @@
 import pygame
 
-WIDTH, HEIGHT = 800, 400
+WIDTH, HEIGHT = 800, 600
 FPS = 60
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
-
+LIST_LEN = 3
+GROUND_BORDER = 115
 
 BG = pygame.transform.scale(pygame.image.load("PYTHON/Learning/Games/flappy_folder/flappy/bg.png"), (WIDTH, HEIGHT))
 GROUND = pygame.image.load("PYTHON/Learning/Games/flappy_folder/flappy/ground.png")
 PIPEDOWN = pygame.image.load("PYTHON/Learning/Games/flappy_folder/flappy/pipedown.png")
 PIPEUP = pygame.transform.rotate(pygame.image.load("PYTHON/Learning/Games/flappy_folder/flappy/pipedown.png"), 180)
 
+class Bird:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.bird_list = []
+        self.bird_count = 0
+        self.animation_speed = 0.1
+        self.jump_count = 7
 
-def draw_birds(screen):
-    birds_list = [] 
-    bird_count = 0
-    animation_speed = 0.1
-    for x in range(3):
-        birds_list.append(pygame.image.load(f"PYTHON/Learning/Games/flappy_folder/flappy/bird{x + 1}.png"))
+    def draw_bird(self, screen):
+        for x in range(LIST_LEN):
+            self.bird_list.append(pygame.image.load(f"PYTHON/Learning/Games/flappy_folder/flappy/bird{x + 1}.png"))
 
-    bird_count += animation_speed
-    # if bird_count > len(birds_list): bird_count = 0
+        self.bird_count += self.animation_speed
+        if self.bird_count > LIST_LEN: self.bird_count = 0
+        screen.blit(self.bird_list[int(self.bird_count)], (self.x, self.y))
 
-    screen.blit(birds_list[int(bird_count)], (100, 100))
-    print(bird_count)
+    def jump(self, keys):
+        self.y += self.jump_count
+        if self.y >= HEIGHT - GROUND_BORDER: self.y = HEIGHT - GROUND_BORDER
+        if self.y < 0: self.y = 0
+
+        if keys[pygame.K_w]:
+            self.y -= self.jump_count
     
-
-def draw_window(screen):
-    screen.blit(BG, (0,0))
-    screen.blit(GROUND, (0, 350))
-
 def main():
+
+    def draw_window(screen):
+        screen.blit(BG, (0,0))
+        screen.blit(GROUND, (0, HEIGHT - (GROUND.get_height() / 2)))
+
     running = True
     clock = pygame.time.Clock()
+    bird = Bird(100, 100)
 
     while running:
         clock.tick(FPS)
@@ -39,9 +52,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+                    
+        keys = pygame.key.get_pressed()
 
+        bird.jump(keys)
         draw_window(SCREEN)
-        draw_birds(SCREEN)
+        bird.draw_bird(SCREEN)
         pygame.display.update()
     pygame.quit()
 

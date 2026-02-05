6b9a20b417792147131edf66e5393f48fe092a77
Martin <wainainam008@gmail.com>
2026-02-05 19:51:27 +0300

    more bird_movement

--- Flappy_Bird.py
+++ Flappy_Bird.py
@@ -1,10 +1,14 @@
 import pygame
 
-WIDTH, HEIGHT = 800, 600
+WIDTH, HEIGHT = 900, 600
 FPS = 60
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 LIST_LEN = 3
 GROUND_BORDER = 115
+BIRD = pygame.image.load("PYTHON/Learning/Games/flappy_folder/flappy/bird1.png")
+BIRD_WIDTH = BIRD.get_height()
+BIRD_HEIGHT = BIRD.get_width()
+BIRD_X, BIRD_Y = 100, 300
 
 BG = pygame.transform.scale(pygame.image.load("PYTHON/Learning/Games/flappy_folder/flappy/bg.png"), (WIDTH, HEIGHT))
 GROUND = pygame.image.load("PYTHON/Learning/Games/flappy_folder/flappy/ground.png")
@@ -18,7 +22,7 @@ class Bird:
         self.bird_list = []
         self.bird_count = 0
         self.animation_speed = 0.1
-        self.jump_count = 7
+        self.jump_count = 9
 
     def draw_bird(self, screen):
         for x in range(LIST_LEN):
@@ -28,11 +32,12 @@ class Bird:
         if self.bird_count > LIST_LEN: self.bird_count = 0
         screen.blit(self.bird_list[int(self.bird_count)], (self.x, self.y))
 
-    def jump(self, keys):
+    def move(self):
         self.y += self.jump_count
         if self.y >= HEIGHT - GROUND_BORDER: self.y = HEIGHT - GROUND_BORDER
         if self.y < 0: self.y = 0
 
+    def jump(self, keys):
         if keys[pygame.K_w]:
             self.y -= self.jump_count
     
@@ -44,18 +49,27 @@ def main():
 
     running = True
     clock = pygame.time.Clock()
-    bird = Bird(100, 100)
+    bird = Bird(BIRD_X, BIRD_Y)
 
     while running:
         clock.tick(FPS)
+        up = False
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
-                    
-        keys = pygame.key.get_pressed()
+                running = False  
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    up = True
+                if event.type == pygame.KEYUP:
+                    up = False
 
-        bird.jump(keys)
+        keys = pygame.key.get_pressed()
+        if up == True:
+            bird.jump(keys)
+        if up == False:
+            bird.move()
+
         draw_window(SCREEN)
         bird.draw_bird(SCREEN)
         pygame.display.update()
